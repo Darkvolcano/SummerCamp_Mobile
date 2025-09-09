8087b6ebf9aeddf0c943369e42f7b2708c642292
Darkvolcano <khangphamchuchi@gmail.com>
2025-09-09 11:03:26 +0700

    update file user_model

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,33 +1,21 @@
 import '../../domain/entities/user.dart';
 
 class UserModel extends User {
-  final int id;
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String phoneNumber;
-
   const UserModel({
-    required this.id,
-    required this.firstName,
-    required this.lastName,
-    required this.email,
-    required this.phoneNumber,
-  }) : super(
-         id: id,
-         firstName: firstName,
-         lastName: lastName,
-         email: email,
-         phoneNumber: phoneNumber,
-       );
+    required super.id,
+    required super.firstName,
+    required super.lastName,
+    required super.email,
+    required super.phoneNumber,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      id: json['userId'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      email: json['email'],
-      phoneNumber: json['phoneNumber'],
+      id: json['userId'] as int,
+      firstName: json['firstName'] as String,
+      lastName: json['lastName'] as String,
+      email: json['email'] as String,
+      phoneNumber: json['phoneNumber'] as String,
     );
   }
 
