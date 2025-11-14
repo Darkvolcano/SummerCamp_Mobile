d71ab8bb345fc9a1f87d49bc4fd2bb013c23f72a
Darkvolcano <khangphamchuchi@gmail.com>
2025-11-14 13:39:55 +0700

    update show status Published, OpenForRegistration, RegistrationClosed for camp list

--- lib/features/camp/presentation/screens/camp_list_screen.dart
+++ lib/features/camp/presentation/screens/camp_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:summercamp/core/enum/camp_status.enum.dart';
 import 'package:summercamp/features/camp/presentation/state/camp_provider.dart';
 import 'package:summercamp/features/camp/presentation/widgets/camp_card.dart';
 import 'package:summercamp/core/config/app_theme.dart';
@@ -29,6 +30,16 @@ class _CampListScreenState extends State<CampListScreen> {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
+    const allowedStatuses = {
+      CampStatus.Published,
+      CampStatus.OpenForRegistration,
+      CampStatus.RegistrationClosed,
+    };
+
+    final filteredCamps = provider.camps
+        .where((camp) => allowedStatuses.contains(camp.status))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -44,7 +55,7 @@ class _CampListScreenState extends State<CampListScreen> {
       ),
       body: provider.loading
           ? const Center(child: CircularProgressIndicator())
-          : provider.camps.isEmpty
+          : filteredCamps.isEmpty
           ? Center(
               child: Text(
                 "Không có trại hè trong mùa hè này",
@@ -56,9 +67,9 @@ class _CampListScreenState extends State<CampListScreen> {
             )
           : ListView.builder(
               padding: const EdgeInsets.all(16),
-              itemCount: provider.camps.length,
+              itemCount: filteredCamps.length,
               itemBuilder: (context, index) {
-                return CampCard(camp: provider.camps[index]);
+                return CampCard(camp: filteredCamps[index]);
               },
             ),
     );
